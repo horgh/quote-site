05fef0d3079cb5c7c1de6f3bd5380901e91e33d1
Will Storey <will@summercat.com>
2017-03-24 15:14:27 -0700

    Show page number more clearly under browse

--- view_browse.php
+++ view_browse.php
@@ -1,20 +1,24 @@
 <?= _get_template('_view_head', array('page_title' => $page_title)); ?>
 
-<p>There are <?= $count_quotes; ?> quotes.</p>
+<p>There are <?= $count_quotes; ?> quotes.
+This is page <?= $page; ?>/<?= $pages; ?>.</p>
 
 <?php foreach ($quotes as $quote): ?>
 	<?php _show_template('_view_quote', array('quote' => $quote)); ?>
 <?php endforeach; ?>
 
 <?php if ($pages > 1): ?>
-	<p>There are <?= $pages; ?> pages of quotes.</p>
 	<p>
 	<?php if ($page > 1): ?>
-		<a href="<?= htmlspecialchars($prev_page_url); ?>">Previous page</a>
+		<a href="<?= htmlspecialchars($prev_page_url); ?>">Page <?= $page-1; ?></a>
 	<?php endif; ?>
 
+	Page <?= $page; ?>
+
 	<?php if ($page !== $pages): ?>
-		<a href="<?= htmlspecialchars($next_page_url); ?>">Next page</a>
+		<a href="<?= htmlspecialchars($next_page_url); ?>">Page <?= $page+1; ?></a>
 	<?php endif; ?>
+
+	(<?= $pages; ?> pages)
 	</p>
 <?php endif; ?>
